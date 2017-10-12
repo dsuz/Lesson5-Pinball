8b4958dc9017ac7e2838ba561f851acdeac0be9a
Daisuke Suzuki <bboydaisuke@gmail.com>
2017-10-12 21:24:11 +0900

    Add exercise (add score for each object)

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -4,21 +4,23 @@ using UnityEngine.UI;
 
 public class BallController : MonoBehaviour
 {
-
     //ボールが見える可能性のあるz軸の最大値
     private float visiblePosZ = -6.5f;
 
     //ゲームオーバを表示するテキスト
     private GameObject gameoverText;
 
-    // Use this for initialization
+    private int m_score;
+
+    /// <summary>スコアを表示するテキスト</summary>
+    [SerializeField] private Text m_scoreText;
+
     void Start()
     {
         //シーン中のGameOverTextオブジェクトを取得
         this.gameoverText = GameObject.Find("GameOverText");
     }
 
-    // Update is called once per frame
     void Update()
     {
         //ボールが画面外に出た場合
@@ -27,5 +29,37 @@ public class BallController : MonoBehaviour
             //GameoverTextにゲームオーバを表示
             this.gameoverText.GetComponent<Text>().text = "Game Over";
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // 衝突したオブジェクトの種類により異なる点数を加算する
+        switch (collision.gameObject.tag)
+        {
+            case "SmallStarTag":
+                AddScore(5);
+                break;
+            case "LargeStarTag":
+                AddScore(10);
+                break;
+            case "SmallCloudTag":
+                AddScore(50);
+                break;
+            case "LargeCloudTag":
+                AddScore(100);
+                break;
+            default:
+                break;
+        }
+    }
+
+    /// <summary>
+    /// 点数を加算し、表示を更新する。
+    /// </summary>
+    /// <param name="score">加算する点数</param>
+    private void AddScore(int score)
+    {
+        m_score += score;
+        if (m_scoreText) m_scoreText.text = "Score: " + m_score;
     }
 }
